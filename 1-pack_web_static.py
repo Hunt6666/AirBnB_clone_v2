d320513a171c33e6e2d069c4130f3116de3d75b0
Hunt6666 <489@holbertonschool.com>
2019-01-15 06:26:09 -0800

    try this

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,8 @@ def do_pack():
     """generates a .tgz archive from the contents of web_static forlder"""
     now = strftime("%Y%M%d%H%m%S")
     try:
-        local("mkdir -p version")
-        fle = "version/web_static_{}.tgz".format(now)
+        local("mkdir -p versions")
+        fle = "versions/web_static_{}.tgz".format(now)
         local("tar -czvf {} web_static/".format(fle))
         return fle
     except:
