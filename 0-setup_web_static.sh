202c6f816fb4ee32cd6fe906e1d680e625c17771
Hunt6666 <489@holbertonschool.com>
2019-01-15 06:53:29 -0800

    bla

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,16 +4,9 @@ apt-get update
 apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-FILEA=/data/web_static/current
-rm -rf /data/web_static/releases/test/current
-if [ -L $FILEA ]; then
-    rm -rf /data/web_static/current
-fi
+echo "Holberton School" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-chmod -R 755 /data/
+chown -hR ubuntu:ubuntu /data/
 FILE=/etc/nginx/sites-available/default
 sed -i '30i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\\
 n\t}\n' $FILE
