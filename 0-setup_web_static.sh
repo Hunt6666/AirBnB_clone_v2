8c6f16d8baf3715ae72f86c382ac10a0ae72c173
Hunt6666 <489@holbertonschool.com>
2019-01-15 05:01:56 -0800

    0fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # sets up web server for the deployment of web static
 apt-get update
-apt-get install nginx
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 FILEA=/data/web_static/current
 rm -rf /data/web_static/releases/test/current
 if [ -L $FILEA ]; then
     rm -rf /data/web_static/current
 fi
-ln -s /data/web_static/current   /data/web_static/releases/test/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
+chmod -R 755 /data/
 FILE=/etc/nginx/sites-available/default
-sed -i '30i\\n\tlocation /hbnb_static/ {\n\t\talias https://mydomainname.tech/h\
-bnb_static/;\n\t}' $FILE
+sed -i '30i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\\
+n\t}\n' $FILE
 service nginx restart
