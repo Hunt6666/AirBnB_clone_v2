6cb61b62d04369904bd71bff19db5d0911a3f3fd
Hunt6666 <489@holbertonschool.com>
2019-01-15 03:25:19 -0800

    0-fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,14 @@ apt-get update
 apt-get install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-ln -s /data/web_static/current /data/web_static/releases/test/
-chmod -R 770 /data/
+touch /data/web_static/releases/test/index.html
+FILEA=/data/web_static/current
+rm -rf /data/web_static/releases/test/current
+if [ -L $FILEA ]; then
+    rm -rf /data/web_static/current
+fi
+ln -s /data/web_static/current   /data/web_static/releases/test/
+chown -R ubuntu:ubuntu /data/
 FILE=/etc/nginx/sites-available/default
 sed -i '30i\\n\tlocation /hbnb_static/ {\n\t\talias https://mydomainname.tech/h\
 bnb_static/;\n\t}' $FILE
