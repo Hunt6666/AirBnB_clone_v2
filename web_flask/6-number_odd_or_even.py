98e95cf9acb0096ca053c9ff0f32d54954fc03c4
Hunt6666 <489@holbertonschool.com>
2019-01-30 11:06:46 -0800

    strict ints

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,10 +42,11 @@ def number_template(n):
         return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
-def odd_or_even(n=None):
+@app.rout('/number_odd_or_even', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
     """displays html page if n is an int """
-    if n % 2 == 0:
+    if (n % 2) == 0:
         t = "even"
         return render_template('6-number_odd_or_even.html', n=n, t=t)
     else:
